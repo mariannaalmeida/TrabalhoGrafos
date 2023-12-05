b67b7fe0a2727d8b3fa3483aa0b28304749fd6bc
mariannaalmeida <96499979+mariannaalmeida@users.noreply.github.com>
2023-12-04 21:28:19 -0300

    Update Aresta.java

--- src/lib/Aresta.java
+++ src/lib/Aresta.java
@@ -1,37 +1,40 @@
 package src.lib;
 
-public class Aresta {
-    private Vertice origem;
-    private Vertice destino;
+package lib;
+
+public class Aresta<T> {
     private float peso;
+    private Vertice<T> destino;
 
-    public Aresta(Vertice origem, Vertice destino, float peso) {
-        this.origem = origem;
-        this.destino = destino;
-        this.peso = peso;
+    // Construtor que recebe um vértice de destino e um peso para inicializar a aresta.
+    public Aresta(Vertice<T> destino, float peso){
+        setPeso(peso);
+        setDestino(destino);
     }
 
-    public Vertice getOrigem() {
-        return origem;
-    }
-
-    public void setOrigem(Vertice origem) {
-        this.origem = origem;
-    }
-
-    public Vertice getDestino() {
-        return destino;
-    }
-
-    public void setDestino(Vertice destino) {
-        this.destino = destino;
-    }
-
-    public float getPeso() {
+    // Obtém o peso associado à aresta.
+    public float getPeso(){
         return peso;
     }
 
-    public void setPeso(float peso) {
+    // Define o peso associado à aresta.
+    public void setPeso(float peso){
         this.peso = peso;
+    }
+
+    // Obtém o vértice de destino associado à aresta.
+    public Vertice<T> getDestino(){
+        return destino;
+    }
+
+    // Define o vértice de destino associado à aresta.
+    public void setDestino(Vertice<T> destino){
+        this.destino = destino;
+    }
+
+    // Sobrescreve o método toString para fornecer uma representação em string da aresta.
+    @Override
+    public String toString() {
+        return destino.getValor() + "; peso: " + this.peso;
     }
 }
